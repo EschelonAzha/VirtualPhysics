f10677cc6b8e225f386669fcec8f8af25a5da7d9
eschelonazha <eschelon@PhysicsMonastery.com>
2023-11-11 17:56:04 -0700

    Partial Update of ZBoson

--- src/main/kotlin/symmetrical/cosmic/_physics/_subatomic/matter/bosons/ZBoson.kt
+++ src/main/kotlin/symmetrical/cosmic/_physics/_subatomic/matter/bosons/ZBoson.kt
@@ -1,11 +1,13 @@
 package symmetrical.cosmic._physics._subatomic.matter.bosons
 
 import symmetrical.cosmic.__recycle.Absorber
+import symmetrical.cosmic._physics._subatomic.balanced.Particle
+import symmetrical.cosmic._physics._subatomic.balanced.values.Field
 import symmetrical.cosmic._physics._subatomic.forces.weak_force.Weak
 import symmetrical.cosmic._physics._subatomic.spatial.ParticleBeam
 
 
-open class ZBoson : Weak {
+open class ZBoson : Particle, Weak {
     object Illuminations {
         public val beam: ParticleBeam = ParticleBeam()
     }
@@ -13,19 +15,28 @@ open class ZBoson : Weak {
     // Spin 1
     // This boson transfers its spin, momentum and energy
 
-    var result:Any? = null
+ //   var result:Any? = null
+
+    private var accepted     :Boolean   = true
+    private var constructing :Boolean   = false
+    private var newValue     :Field     = Field();
+    private var oldValue     :Field     = Field()
+    private var reasonCode   :Int       = 0
+    private var reason       :String    = ""
+
     constructor() {
     }
-    public fun getIlluminations() : ParticleBeam {
+
+    public override fun getIlluminations() : ParticleBeam {
         return Illuminations.beam
     }
     private fun getLocalClassId() : String {
         return Absorber.getClassId(ZBoson::class)
     }
-    open fun getClassId() : String {
+    override fun getClassId() : String {
         return getLocalClassId()
     }
-    fun i() : Weak {
+    fun i(newValue: Field, constructing:Boolean = false) : ZBoson {
         return this
     }
 }
