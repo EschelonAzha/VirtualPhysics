e64c8006692c5d387b43b99cb79ff531da474cf8
eschelonazha <eschelon@PhysicsMonastery.com>
2023-11-20 11:57:34 -0700

    Fixup all Constructors

--- src/main/kotlin/symmetrical/cosmic/wormholes/messaging/entanglement/IQuantumEntanglement.kt
+++ src/main/kotlin/symmetrical/cosmic/wormholes/messaging/entanglement/IQuantumEntanglement.kt
@@ -1,15 +1,11 @@
 package symmetrical.cosmic.wormholes.messaging.entanglement
 
 import symmetrical.cosmic._physics._subatomic.balanced.Particle
-import symmetrical.cosmic._physics._subatomic.fermion.Fermion
 import symmetrical.cosmic._physics._subatomic.spatial.ParticleBeam
 
 interface IQuantumEntanglement {
-
-    fun getAbsorberId() : String
-    fun getClassId() : String
-
-    fun getIlluminations() : ParticleBeam
-    fun setSelf(self: Particle) : Fermion
-
+    fun getAbsorberId()         : String
+    fun getClassId()            : String
+    fun getIlluminations()      : ParticleBeam
+    fun setSelf(self: Particle) : IQuantumEntanglement
 }
--- src/main/kotlin/symmetrical/cosmic/wormholes/messaging/entanglement/QuantumEntanglement.kt
+++ src/main/kotlin/symmetrical/cosmic/wormholes/messaging/entanglement/QuantumEntanglement.kt
@@ -1,12 +1,38 @@
 package symmetrical.cosmic.wormholes.messaging.entanglement
 
+import symmetrical.cosmic.__recycle.Absorber
+import symmetrical.cosmic._physics._subatomic.balanced.Particle
+import symmetrical.cosmic._physics._subatomic.fermion.Fermion
 import symmetrical.cosmic._physics._subatomic.spatial.ParticleBeam
+import kotlin.reflect.KClass
 
 class QuantumEntanglement : IQuantumEntanglement {
     object Illuminations {
         public val beam: ParticleBeam = ParticleBeam()
     }
+
+
+    var self:Any? = null
+
+    override fun setSelf(self: Particle) : QuantumEntanglement {
+        this.self = self
+        return this
+    }
+    override fun getAbsorberId() : String {
+        return Absorber.getClassId(getKClass())
+    }
+
+    override fun getClassId() : String {
+        return Absorber.getClassId(getKClass())
+    }
     public override fun getIlluminations() : ParticleBeam {
         return Illuminations.beam
+    }
+
+    private  fun getKClass() : KClass<*> {
+        if (self == null) {
+            return this::class
+        }
+        return (self as Particle)::class
     }
 }
