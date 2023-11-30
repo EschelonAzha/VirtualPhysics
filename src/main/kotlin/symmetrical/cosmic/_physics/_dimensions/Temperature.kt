f6eb28a51ef67bcdfea4af6740e0556a6fadd042
eschelonazha <eschelon@PhysicsMonastery.com>
2023-11-30 14:58:59 -0700

    Removed Cosmic

--- src/main/kotlin/symmetrical/cosmic/_physics/_dimensions/Temperature.kt
+++ src/main/kotlin/symmetrical/cosmic/_physics/_dimensions/Temperature.kt
@@ -1,5 +1,6 @@
 package symmetrical.cosmic._physics._dimensions
 
+import symmetrical.cosmic.__recycle.Absorber
 import symmetrical.cosmic.__transpectors.transpectors.Photons
 import symmetrical.cosmic.__transpectors.transpectors.Strings
 import symmetrical.cosmic._physics._subatomic.balanced.Particle
@@ -10,33 +11,8 @@ import symmetrical.cosmic._physics._subatomic.bosons.Photon
 import symmetrical.cosmic._physics._subatomic.luminescent.IAntiMatter
 import symmetrical.cosmic._physics._subatomic.luminescent.AntiMatter
 
-class Temperature (
-    private val antiMatter: IAntiMatter = AntiMatter(Temperature::class),
-) :
-    IAntiMatter by antiMatter,
-    Emitter
-{
-    constructor() : this(
-        AntiMatter(Temperature::class),
-    )
-    object Static {
-        const val NULL      = "0"
-        const val BOOLEAN   = "B"
-        const val BYTE      = "1"
-        const val INT       = "I"
-        const val DOUBLE    = "D"
-        const val STRING1   = "s"
-        const val STRING2   = "S"
-        const val STRING3   = "!"
-        const val VECTOR    = "V"
+class Temperature : Emitter {
 
-        const val NUMBERS_LTH = 1
-        const val STRING1_LTH = 1
-        const val STRING2_LTH = 2
-        const val STRING3_LTH = 3
-        const val VECTOR_LTH  = 3
-
-    }
 
     private var field      : Field = Field()
 
@@ -45,50 +21,49 @@ class Temperature (
     }
 
     override fun absorb(photon: Photon) : Photon {
-        antiMatter.check(photon);
 
-        var (type, lthValue) = Strings.remainder(Static.NUMBERS_LTH, photon.propagate().radiate())
-        if (type == Static.NULL) {
+        var (type, lthValue) = Strings.remainder(Field.Static.NUMBERS_LTH, photon.propagate().radiate())
+        if (type == Field.Static.NULL) {
             field.setValue(null)
             return Photon(lthValue)
         }
-        if (type == Static.BOOLEAN) {
-            var (value, remainder) = Photons.parse(Static.NUMBERS_LTH, lthValue)
+        if (type == Field.Static.BOOLEAN) {
+            var (value, remainder) = Photons.parse(Field.Static.NUMBERS_LTH, lthValue)
             field.setValue(value == "true")
             return Photon(remainder)
         }
-        if (type == Static.BYTE) {
-            var (value, remainder) = Photons.parse(Static.NUMBERS_LTH, lthValue)
+        if (type == Field.Static.BYTE) {
+            var (value, remainder) = Photons.parse(Field.Static.NUMBERS_LTH, lthValue)
             field.setValue(value)
             return Photon(remainder)
         }
-        if (type == Static.INT) {
-            var (value, remainder) = Photons.parse(Static.NUMBERS_LTH, lthValue)
+        if (type == Field.Static.INT) {
+            var (value, remainder) = Photons.parse(Field.Static.NUMBERS_LTH, lthValue)
             field.setValue(value.toInt())
             return Photon(remainder)
         }
-        if (type == Static.DOUBLE) {
-            var (value, remainder) = Photons.parse(Static.NUMBERS_LTH, lthValue)
+        if (type == Field.Static.DOUBLE) {
+            var (value, remainder) = Photons.parse(Field.Static.NUMBERS_LTH, lthValue)
             field.setValue(value.toDouble())
             return Photon(remainder)
         }
-        if (type == Static.STRING1) {
-            var (value, remainder) = Photons.parse(Static.STRING1_LTH, lthValue)
+        if (type == Field.Static.STRING1) {
+            var (value, remainder) = Photons.parse(Field.Static.STRING1_LTH, lthValue)
             field.setValue(value)
             return Photon(remainder)
         }
-        if (type == Static.STRING2) {
-            var (value, remainder) = Photons.parse(Static.STRING2_LTH, lthValue)
+        if (type == Field.Static.STRING2) {
+            var (value, remainder) = Photons.parse(Field.Static.STRING2_LTH, lthValue)
             field.setValue(value)
             return Photon(remainder)
         }
-        if (type == Static.STRING3) {
-            var (value, remainder) = Photons.parse(Static.STRING3_LTH, lthValue)
+        if (type == Field.Static.STRING3) {
+            var (value, remainder) = Photons.parse(Field.Static.STRING3_LTH, lthValue)
             field.setValue(value)
             return Photon(remainder)
         }
-        if (type == Static.VECTOR) {
-            var (value, remainder) = Photons.parse(Static.VECTOR_LTH, lthValue)
+        if (type == Field.Static.VECTOR) {
+            var (value, remainder) = Photons.parse(Field.Static.VECTOR_LTH, lthValue)
             field.setValue(value)
             return Photon(remainder)
         }
@@ -107,29 +82,33 @@ class Temperature (
         return getField().getValue()
     }
     private fun radiate() : String {
-        val prefix = antiMatter.getClassId()+field.getType()
-        if (field.getType() == Static.NULL)
+        val prefix = getClassId()+field.getType()
+        if (field.getType() == Field.Static.NULL)
             return prefix
-        if (field.getType() == Static.BOOLEAN)
+        if (field.getType() == Field.Static.BOOLEAN)
             return prefix+ Photon.toPhoton1(field.getValue())
-        if (field.getType() == Static.BYTE)
+        if (field.getType() == Field.Static.BYTE)
             return prefix+ Photon.toPhoton1(field.getValue())
-        if (field.getType() == Static.INT)
+        if (field.getType() == Field.Static.INT)
             return prefix+ Photon.toPhoton1(field.getValue())
-        if (field.getType() == Static.DOUBLE)
+        if (field.getType() == Field.Static.DOUBLE)
             return prefix+ Photon.toPhoton1(field.getValue())
-        if (field.getType() == Static.STRING1)
+        if (field.getType() == Field.Static.STRING1)
             return prefix+ Photon.toPhoton1(field.getValue())
-        if (field.getType() == Static.STRING2)
+        if (field.getType() == Field.Static.STRING2)
             return prefix+ Photon.toPhoton2(field.getValue())
-        if (field.getType() == Static.STRING3)
+        if (field.getType() == Field.Static.STRING3)
             return prefix+ Photon.toPhoton3(field.getValue())
 
         return prefix+ Photon.toPhoton3(field.getValue())
     }
 
-    override fun getClassId() : String {
-        return antiMatter.getClassId()
+    private fun getLocalClassId() : String {
+        return Absorber.getClassId(Temperature::class)
+    }
+
+    public fun getClassId() : String {
+        return getLocalClassId()
     }
 
     fun temperature() : Any? {
