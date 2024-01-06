9aac62ed52e4355e9811b5dd3096a5e47c97c9c7
eschelonazha <sdesofi@logicalsociety.com>
2024-01-06 17:26:58 -0500

    replace accidental license copy

--- src/main/kotlin/symmetrical/cosmic/transpectors/transpectors/Bytes.kt
+++ src/main/kotlin/symmetrical/cosmic/transpectors/transpectors/Bytes.kt
@@ -1,4 +1,22 @@
 package symmetrical.cosmic.transpectors.transpectors
+/*
+ * This file is part of Virtual Physics.
+ *
+ * Copyright (C) [2024] Stephen R. DeSofi AKA Eschelon Azha
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import asymmetrical.physics.machine.transpectors.ABytes
 
