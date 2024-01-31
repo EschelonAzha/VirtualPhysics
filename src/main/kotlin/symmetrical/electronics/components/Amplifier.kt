c59b3319abbf5cb3335e313d412150129e5d5f04
eschelonazha <sdesofi@logicalsociety.com>
2024-01-30 20:29:55 -0500

    setting up for future electronics work

--- src/main/kotlin/symmetrical/electronics/components/Amplifier.kt
+++ src/main/kotlin/symmetrical/electronics/components/Amplifier.kt
@@ -1,4 +1,23 @@
 package symmetrical.electronics.components
+/*
+ * This file is part of Virtual Physics.
+ *
+ * Copyright (C) [2024] Stephen R. DeSofi AKA Eschelon Azha
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 // for more information see:   https://en.wikipedia.org/wiki/Error_amplifier_(electronics)
 class Amplifier {
